5c1b61667075e18cd4b6421fc2dd4778e2f0f7f7
Sébastien Serre <sserre.bx@gmail.com>
2017-06-14 21:52:20 +0200

    vectorized loop with fonction arguments

--- main.c
+++ main.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
 
-int mat1[4][4] = {
-    {2,0,0,1},
-    {0,9,1,0},
-    {0,1,0,0},
-    {1,0,0,0}
-};
-
-int mat2[4][4] = {
-    {1,0,0,0},
-    {0,1,0,0},
-    {0,0,1,0},
-    {0,0,0,1}
-};
-
-int result[4][4];
+struct mat4 { float v[4][4]; };
 
 void
-foo() {
+multMat4(
+        struct mat4* __restrict__ m1,
+        struct mat4* __restrict__ m2,
+        struct mat4* __restrict__ m3) {
+
     int x, y, z, res;
 
     for (x=0; x<4; x++) {
         for (y=0; y<4; y++) {
             res = 0;
             for (z=0; z<4; z++) {
-                res += mat1[x][z] * mat2[z][y];
+                res += m1->v[x][z] * m2->v[z][y];
             }
-            result[x][y] = res;
+            m3->v[x][y] = res;
         }
     }
 }
@@ -35,10 +25,26 @@ int
 main(int argc, char** argv) {
     int i;
 
-    foo();
+    struct mat4 result;
+    struct mat4 m1 = {{
+        {2.0f,9.0f,8.0f,1.0f},
+        {0.0f,9.0f,1.0f,0.0f},
+        {0.0f,1.0f,0.0f,0.0f},
+        {1.0f,0.0f,0.0f,0.0f}
+    }};
+
+    struct mat4 m2= {{
+        {1.0f,0.0f,0.0f,0.0f},
+        {0.0f,1.0f,0.0f,0.0f},
+        {0.0f,0.0f,1.0f,0.0f},
+        {0.0f,0.0f,0.0f,1.0f}
+    }};
+
+    multMat4(&m1, &m2, &result);
 
     for (i=0; i< 4; i++) {
-        printf("%d %d %d %d\n", result[i][0],result[i][1], result[i][2], result[i][3]);
+        printf("%.0f %.0f %.0f %.0f\n", result.v[i][0], result.v[i][1], result.v[i][2], result.v[i][3]);
     }
+
     return 0;
 }
